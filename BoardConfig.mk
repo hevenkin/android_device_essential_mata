ac7c1689b9c0db971a8ed409a468e1ae39c59c1c
hevenkin <applexhy78@163.com>
2022-10-19 16:36:21 +0800

    mata: Track our new defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,7 +89,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/essential/msm8998
-TARGET_KERNEL_CONFIG := lineageos_mata_defconfig
+TARGET_KERNEL_CONFIG := mata_defconfig
 
 # Kernel additional flags
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
